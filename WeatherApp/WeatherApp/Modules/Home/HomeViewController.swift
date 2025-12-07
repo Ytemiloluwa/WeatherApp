ccfb1e29e5f4474ce299e50e685fa8ee972304e4
Ytemiloluwa <ytemiloluwa@gmail.com>
2025-12-07 13:39:14 +0100

    [chore: connected IBoulets to HomeViewController]

--- WeatherApp/WeatherApp/Modules/Home/HomeViewController.swift
+++ WeatherApp/WeatherApp/Modules/Home/HomeViewController.swift
@@ -6,3 +6,20 @@
 //
 
 import UIKit
+
+class HomeViewController: UIViewController {
+    
+    @IBOutlet weak var cityTextField: UITextField!
+    @IBOutlet weak var searchButton: UIButton!
+    @IBOutlet weak var saveFavoriteButton: UIButton!
+    @IBOutlet weak var activityIndicator: UIActivityIndicatorView!
+    @IBOutlet weak var errorLabel: UILabel!
+    
+    
+    
+    override func viewDidLoad() {
+        
+        super.viewDidLoad()
+        
+    }
+}
